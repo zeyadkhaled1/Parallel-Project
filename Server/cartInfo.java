27af21be00fb3b9bd666800c5a88902e6adf1add
ZyadZ2000 <85132955+ZyadZ2000@users.noreply.github.com>
2022-07-01 23:57:03 +0200

    Create cartInfo.java

---
+++ Server/cartInfo.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+public class cartInfo implements Serializable {
+    String id;
+    String userName;
+    String price;
+    String amount;
+    String itemName;
+    String total;
+    
+}
