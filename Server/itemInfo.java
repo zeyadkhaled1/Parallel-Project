40aa67fa5ad12323ec001df52dd7d30b724fc0a6
nadaalaa811 <85295041+nadaalaa811@users.noreply.github.com>
2022-07-02 00:00:48 +0200

    Create itemInfo.java

---
+++ Server/itemInfo.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+public class itemInfo implements Serializable {
+    String id;
+    String name;
+    String price;
+    String quantity;
+    String category;
+    String inCart;
+    
+}
