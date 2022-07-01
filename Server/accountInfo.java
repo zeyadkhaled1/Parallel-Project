0d2bf8a76fc02391af326a44adbc52983e207543
ZyadZ2000 <85132955+ZyadZ2000@users.noreply.github.com>
2022-07-01 23:56:28 +0200

    Create accountInfo.java

---
+++ Server/accountInfo.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+ 
+public class accountInfo implements Serializable {  
+    String userName;
+    String email;
+    String address;
+    String cash;
+    String password; 
+}
